2fc0657c034bf4b45ee3037f7c46e836b91dad5f
florenceluo <99026236+florenceluo@users.noreply.github.com>
2022-02-22 23:02:54 +0000

    homework1, submission1
    hopefully the labelling helps grading :))

--- bus.py
+++ bus.py
@@ -4,4 +4,45 @@
 #
 #######################################################
 
+current= int(input ("What is the departure time(in HHMM)?"))
+dist= int(input("How far (in km) is the bus ride? "))
+stops= int(input("How many stops is the bus making?"))
 
+currenth,currentm = divmod(current,100)
+#print(currenth) #currentm is just the number in the HOUR of the departure time 
+#print(currentm) #currentm is just the number in the MINUTES of the departure time 
+
+travh,travleftm= divmod(dist,40)
+travm= int(travleftm/40*60) 
+if travleftm % 2 == 0: #this is saying if the remainder is even then simple use the remainder to find the minutes needed and then set travel seconds to 0
+  travs=0
+else: #this is saying is the remainder is odd, then there will need to be an additional seconds to take into account, which we will assume is 30 seconds given that all distances are given in integers
+  travs=30
+
+#print(travh) #travh is the HOURS need to travel the distance at a rate of 40km/hr
+#print (travleftm) #travleftm is the remainder, need to divide by 40km/hr to get fraction of hour, then multiply by 60min/hour to get minutes
+#print(travm) #travm is the MINUTES need to travel the distance at a rate of 40km/hr
+#print(travs) #travs is the SECONDS needed to travel the distance at a rate of 40 km/hr
+
+
+stoptime=stops*30 #stoptime is how many seconds taken if each stop is 30 seconds
+stopmin,stopsec=divmod(stoptime,60)
+#print(stopmin) 
+#print(stopsec)
+
+tempsec=stopsec+travs
+additionalmin,totalsec=divmod(tempsec,60)
+#print(additionalmins) #here additional minutes included in case the seconds for stopping and traveling exceed 60
+tempmins=int(currentm+travm+stopmin+additionalmin)
+#print(mins) #here tempmins is the temporary minute variable, or the total minutes needed to travel and to stop added to the current time
+additionalhour,totalmin=divmod(tempmins,60)
+#print(additionalhour)#additionalhour is included here in case the number of minutes exceed 60, in which case the additional hour(s) will just be added on to the total hour
+#print(totalmin)
+totalhour=currenth+travh+additionalhour
+if totalhour>=24:
+  totalhour= totalhour-24
+
+
+
+
+print("Your arrival time will be " + f"{totalhour:02d}"+ ":" + f"{totalmin:02d}"+ ":"+ f"{totalsec:02d}")
--- face.py
+++ face.py
@@ -6,3 +6,137 @@
 
 import turtle
 
+#name our turtle "t"
+t = turtle.Turtle()
+sc = turtle.Screen()
+#sc.mode('world')
+t.getscreen()
+sc.title("Little House on Prairie")
+sc.setworldcoordinates(0, 0, 400, 400)
+t.speed('fastest')
+turtle.colormode(255)
+
+#setting background color
+sc.bgcolor(152,226,240)
+
+#making the square hill 
+t.penup()
+t.goto(-10, 180)
+t.pendown()
+t.fillcolor(89,201,97)
+t.begin_fill()
+t.pencolor (89,201,97)
+t.pensize(5)
+t.setheading(0)
+for side in range (4):
+  t.forward(410)
+  t.right(90)
+t.end_fill()
+t.penup()
+
+#making the sun, a mixture of circles and triangles
+t.goto(40,350)
+t.pendown()
+t.fillcolor('yellow')
+t.begin_fill()
+t.pencolor (245,190,38)
+t.pensize(2)
+t.setheading(90)
+for repeats in range(20):
+    t.forward(10)
+    t.left(18)
+    for sides in range(3):
+        t.forward(50)
+        t.right(120)
+t.end_fill()
+t.penup()
+
+#making a house
+t.goto(200,200)
+t.pendown()
+t.pencolor (245,190,38)
+t.pensize(2)
+t.fillcolor(247,218,173)
+t.begin_fill()
+t.setheading(180) #make turtle face left
+for sides in range (3): #draw triangular part of roof
+  t.forward (80)
+  t.right(120)
+for sides in range(2): #draw the rectangular entry face
+  t.forward (80)
+  t.left(90)
+  t.forward(100)
+  t.left(90)
+t.setheading (25)
+for sides in range (2):#draw parallelogram for the roof
+  t.forward(130)
+  t.left(95)
+  t.forward(80)
+  t.left(85)
+for sides in range (2): #draw the retangular body of the house
+  t.forward(130)
+  t.right(115)
+  t.forward(100)
+  t.right(65)
+t.penup()
+t.end_fill()
+
+#make a doorway
+t.goto (140,100)
+t.pendown()
+t.setheading (90)
+t.pencolor(193,57,37)
+t.fillcolor(246,65,37)
+t.begin_fill()
+for side in range (2):
+  t.forward (60)
+  t.right(90)
+  t.forward(40)
+  t.right(90)
+t.end_fill()
+t.penup()
+
+#make a doorhandle
+t.goto (178,130)
+t.pendown()
+t.setheading (90)
+t.pencolor(236,163,16)
+t.fillcolor(249,182,48)
+t.begin_fill()
+t.circle(4)
+t.end_fill()
+t.penup()
+
+#make roof window
+t.goto (176,224)
+t.pendown()
+t.fillcolor(219,209,190)
+t.pencolor(211,219,220)
+t.begin_fill()
+t.circle(15)
+t.end_fill()
+t.penup()
+
+#make a cross hatch for window
+for n in range (4):
+  t.goto(162,224)
+  t.pendown()
+  t.setheading(n*90)
+  t.forward(8)
+t.penup()
+
+#make a window!
+t.goto(215,185)
+t.pendown()
+t.begin_fill()
+t.setheading (25)
+for sides in range (2): 
+  t.forward(100)
+  t.right(115)
+  t.forward(60)
+  t.right(65)
+t.end_fill()
+t.penup()
+
+turtle.done()
+
